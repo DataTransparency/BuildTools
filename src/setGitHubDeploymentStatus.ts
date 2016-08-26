ba5d5ddc2df8c4551d9b1bda3f8dadeb75467b1b
jamesjwood <jamesjwood@msn.com>
2016-08-26 10:04:14 -0400

    Add assertion that the API is created

--- src/setGitHubDeploymentStatus.ts
+++ src/setGitHubDeploymentStatus.ts
@@ -8,9 +8,14 @@ let statuses: Array<string> = ["pending", "success", "failure", "error"];
 class SetGitHubDeploymentStatus implements ISetGitHubDeploymentStatus {
     private _myAPI: IGitHubAPI;
     public constructor(@inject(TYPES.iGitHubAPI) myAPI: IGitHubAPI){
+        assert(myAPI);
         this._myAPI = myAPI;
     }
     public execute(user: string, repo: string, id: number, state: string, description: string) {
+        assert(user);
+        assert(user);
+        assert(user);
+        assert(user);
         if (statuses.indexOf(state)<0) {
             throw new Error("Invalid State: " + state);
         }
